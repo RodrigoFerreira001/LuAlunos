693225b801eaaf742c564a35d5fe1b78b312dd6b
SavyoToledo <savyomachado@gmail.com>
2017-12-13 22:43:02 -0200

    ATT Alunos

--- Aluno.lua
+++ Aluno.lua
@@ -1,20 +1,21 @@
 -- Meta class
-Aluno = {ra = 0, nome = "", email = "", curso = 0}
+local classes = require "classes"
+local Aluno = classes.class()
 
 -- Derived class method new
-function Aluno:new (ra, nome, email, curso)
-   local acnt = {}
-   setmetatable(acnt, self)
-   self.__index = self
-   self.ra = ra or 0
-   self.nome = nome or ""
-   self.email = email or ""
-   self.curso = curso or 0
-   return acnt
+function Aluno:init (ra, nome, email, curso)
+   self.ra = ra
+   self.nome = nome
+   self.email = email
+   self.curso = curso
 end
 
 function Aluno:set_ra(ra)
 	self.ra = ra
+end
+
+function Aluno:get_ra()
+	return self.ra
 end
 
 function Aluno:set_nome(nome)
@@ -25,11 +26,20 @@ function Aluno:get_nome()
 	return self.nome
 end
 
-
 function Aluno:set_email(email)
 	self.email = email
+end
+
+function Aluno:get_email()
+	return self.email
 end
 
 function Aluno:set_curso(curso)
 	self.curso = curso
 end
+
+function Aluno:get_curso()
+	return self.curso
+end
+
+return Aluno
--- main_rodrigo.lua
+++ main_rodrigo.lua
@@ -1,4 +1,16 @@
-require "Aluno"
+local Aluno = require "Aluno"
+local classes = require "classes"
 
-novo_aluno = Aluno:new(10, "Rodrigo Ferreira", "rodrigoferreira001@hotmail.com", 5)
-print(novo_aluno.nome)
+novo_aluno = Aluno.new(10, "Rodrigo Ferreira", "rodrigoferreira001@hotmail.com", 5)
+novo_aluno2 = Aluno.new(15, "Savyo", "doido@gmail.com", 13)
+-- a = {novo_aluno,novo_aluno2}
+a = {}
+table.insert(a,novo_aluno)
+table.insert(a,novo_aluno2)
+for i,aluno in ipairs(a) do
+	print(aluno:get_nome())
+	print(aluno:get_ra())
+	print(aluno:get_email())
+	print(aluno:get_curso())
+end
+-- print(novo_aluno.nome)
