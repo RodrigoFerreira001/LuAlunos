dec0a8a43de671b2a89ebe29c9daadbeb405bec5
SavyoToledo <savyomachado@gmail.com>
2017-12-13 23:20:49 -0200

    ATT inserir e alterar alunos

--- main_rodrigo.lua
+++ main_rodrigo.lua
@@ -4,7 +4,11 @@ local Curso = require "Curso"
 local Disciplina = require "Disciplina"
 
 disciplinas = {}
-disciplina_index = 0
+disciplina_index = 1
+
+alunos = {}
+aluno_index = 1
+
 
 option = 0
 while(true) do
@@ -61,6 +65,27 @@ while(true) do
 				io.read()
 
 			elseif(option == 2) then
+				os.execute("clear")
+				print("Entre com os dados do aluno")
+				print("RA:")
+				local ra = io.read()
+
+				print("Nome:")
+				local nome = io.read()
+
+				print("Email:")
+				local email = io.read()
+
+				print("Curso:")
+				local curso = io.read()
+
+				local aluno = Aluno.new(ra, nome, email, curso)
+
+				table.insert(alunos, aluno)
+
+				print("Aluno "..nome.." inserido com sucesso!")
+				io.read()
+
 			elseif(option == 3) then
 			elseif(option == 4) then
 			elseif(option == 5) then
@@ -83,7 +108,70 @@ while(true) do
 			option = tonumber(option)
 
 			if(option == 1) then
+				print("Qual disciplina você deseja alterar? (-1 para cancelar)")
+				for i, element in pairs(disciplinas) do
+					print(i, element:get_nome())
+				end
+
+				local id = tonumber(io.read())
+				if(id == -1) then
+					break
+				end
+
+				print("Entre com os dados da disciplina")
+				print("Código:")
+				local codigo = io.read()
+
+				print("Nome:")
+				local nome = io.read()
+
+				print("Carga Horária:")
+				local carga_horaria = io.read()
+
+				print("Valor:")
+				local valor = io.read()
+
+				disciplinas[id]:set_codigo(codigo)
+				disciplinas[id]:set_nome(nome)
+				disciplinas[id]:set_carga_horaria(carga_horaria)
+				disciplinas[id]:set_valor(valor)
+
+				print("Disciplina "..nome.." alterada com sucesso!")
+				io.read()
+
 			elseif(option == 2) then
+				print("Qual aluno você deseja alterar? (-1 para cancelar)")
+				for i, element in pairs(alunos) do
+					print(i, element:get_nome())
+				end
+
+				local id = tonumber(io.read())
+				if(id == -1) then
+					break
+				end
+
+				print("Entre com os dados do aluno")
+				print("RA:")
+				local ra = io.read()
+
+				print("Nome:")
+				local nome = io.read()
+
+				print("Email:")
+				local email = io.read()
+
+				print("Curso:")
+				local curso = io.read()
+
+
+				alunos[id]:set_ra(ra)
+				alunos[id]:set_nome(nome)
+				alunos[id]:set_email(email)
+				alunos[id]:set_curso(curso)
+
+				print("Aluno "..nome.." alterado com sucesso!")
+				io.read()
+				
 			elseif(option == 3) then
 			elseif(option == 4) then
 			elseif(option == 5) then
